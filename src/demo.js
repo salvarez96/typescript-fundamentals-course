2f8c075c9a01f847884361cfa986895dbffb50b9
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-01-26 18:11:35 -0500

    fix: debug code's semantics with @ts-check suggestions

--- src/demo.js
+++ src/demo.js
@@ -7,33 +7,34 @@
 
   async function getProducts() {
     const rta = await fetch('http://api.escuelajs.co/api/v1/products', {
-      mehtod: 'GET'
+      method: 'GET'
     });
-    const data = await rta.parseJson();
+    const data = await rta.json();
     products.concat(data);
   }
   function getTotal() {
-    const total = 0;
-    for (const i = 0; i < products.length(); i++) {
+    let total = 0;
+    for (let i = 0; i < products.length; i++) {
       total += products[i].prize;
     }
     return total;
   }
   function addProduct(index) {
-    if (getTotal <= limit) {
+    const total = getTotal();
+    if (total <= limit) {
       myCart.push(products[index]);
     }
   }
 
-  await getProducts();
-  addProducto(1);
-  addProducto(2);
+  getProducts();
+  addProduct(1);
+  addProduct(2);
   const total = getTotal();
   console.log(total);
   const person = {
     name: 'Nicolas',
     lastName: 'Molina'
   }
-  const rta = person +  limit;
+  const rta = {...person, limit: limit};
   console.log(rta);
-});
+})();
