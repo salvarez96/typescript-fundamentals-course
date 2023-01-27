ac55414f4b00c7d851e2840bd39a38b7c7df5f61
Santiago Alvarez <santiagoalvarez9617@gmail.com>
2023-01-26 21:01:31 -0500

    Class 8: learn about type inference and IIFE

---
+++ src/03-demo.ts
@@ -0,0 +1,10 @@
+(() => {
+  let myProductName = 'Product 1';
+  let myProductPrice = 10;
+
+  myProductName = 'Product 2';
+  // myProductName = 2;
+
+
+  const myProductName2 = 'Product 2';
+})();
---
+++ src/03-typing.ts
@@ -0,0 +1,9 @@
+let myProductName = 'Product 1';
+let myProductPrice = 10;
+
+myProductName = 'Product 2';
+// myProductName = 2;
+
+
+const myProductName2 = 'Product 2';
+
